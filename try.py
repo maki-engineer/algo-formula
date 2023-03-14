d0db923ac67a45e0e04e7461588ba001ba6d70f0
maki-engineer <ryohei-81@outlook.jp>
2023-03-14 16:15:08 +0900

    add: 全探索の問題を解いた

--- try.py
+++ try.py
@@ -1,10 +1,10 @@
-N, V = map(int, input().split())
-A    = list(map(int, input().split()))
+N = int(input())
+A = list(map(int, input().split()))
 
 count = 0
 
 for a in A:
-  if a == V:
+  if a > 0:
     count += 1
 
 print(count)
