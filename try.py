a7f9a5aecbb006abe6a97c6ac7b9a6ae44ec689c
maki-engineer <ryohei-81@outlook.jp>
2023-03-18 07:12:48 +0900

    add: 全探索の問題を解いた

--- try.py
+++ try.py
@@ -1,3 +1,11 @@
-S, c = input(), input()
+S = input()
 
-print("Yes" if c in S else "No")
+str_index = len(S) - 1
+to         = len(S) // 2
+
+for i in range(to):
+  if S[i] != S[str_index - i]:
+    print("No")
+    break
+else:
+  print("Yes")
