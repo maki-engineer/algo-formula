6569a40ad65844614ed45b0747355ab09f35b088
maki-engineer <ryohei-81@outlook.jp>
2023-03-19 07:22:11 +0900

    add: 全探索の問題を解いた

--- try.py
+++ try.py
@@ -1,3 +1,23 @@
-S, T = input(), input()
+N = int(input())
+A = list(map(int, input().split()))
 
-print("Yes" if T in S else "No")
+count = 0
+
+def check_prime_number(num):
+  check_not_prime = False
+
+  if num == 1:
+    return True
+
+  for n in range(2, num):
+    if num % n == 0:
+      check_not_prime = True
+      break
+
+  return check_not_prime
+
+for a in A:
+  if not check_prime_number(a):
+    count += 1
+
+print(count)
