5f1f8abda06a4c2ffcef862409a65ad3831bde15
maki-engineer <ryohei-81@outlook.jp>
2023-03-20 07:15:13 +0900

    add: 全探索の問題を解いた

--- try.py
+++ try.py
@@ -1,24 +1,11 @@
-N = int(input())
+N, M = map(int, input().split())
+A    = list(map(int, input().split()))
+B    = list(map(int, input().split()))
 
 count = 0
 
-# 回文数を調べる関数
-def check_palindrome(string):
-  is_palindrome = True
-  half_index    = len(string) // 2
-  to_index      = len(string) - 1
-
-  for s in range(half_index):
-    if string[s] != string[to_index - s]:
-      is_palindrome = False
-      break
-
-  return is_palindrome
-
-for _ in range(N):
-  check_str = input()
-
-  if check_palindrome(check_str):
-    count += 1
+for ai in range(N):
+  for bi in range(M):
+    if A[ai] > B[bi]: count += 1
 
 print(count)
