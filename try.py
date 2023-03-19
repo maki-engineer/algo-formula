50b1c0cac64f1b812497f958a876a006f6de907f
maki-engineer <ryohei-81@outlook.jp>
2023-03-20 07:20:41 +0900

    add: 全探索の問題を解いた

--- try.py
+++ try.py
@@ -1,11 +1,13 @@
-N, M, K = map(int, input().split())
+X, Y, Z = map(int, input().split())
 A    = list(map(int, input().split()))
 B    = list(map(int, input().split()))
+C    = list(map(int, input().split()))
 
 count = 0
 
-for ai in range(N):
-  for bi in range(M):
-    if A[ai] + B[bi] == K: count += 1
+for ai in range(X):
+  for bi in range(Y):
+    for ci in range(Z):
+      if A[ai] + B[bi] == C[ci]: count += 1
 
 print(count)
