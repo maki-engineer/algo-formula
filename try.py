b34d60a5834f2eadd1f209d5af7c55c89ffe65d9
maki-engineer <ryohei-81@outlook.jp>
2023-03-20 07:17:11 +0900

    add: 全探索の問題を解いた

--- try.py
+++ try.py
@@ -1,4 +1,4 @@
-N, M = map(int, input().split())
+N, M, K = map(int, input().split())
 A    = list(map(int, input().split()))
 B    = list(map(int, input().split()))
 
@@ -6,6 +6,6 @@ count = 0
 
 for ai in range(N):
   for bi in range(M):
-    if A[ai] > B[bi]: count += 1
+    if A[ai] + B[bi] == K: count += 1
 
 print(count)
