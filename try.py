3a0f59b687d8282e3d4bff19e2f09efb5c90004d
maki-engineer <ryohei-81@outlook.jp>
2023-03-21 07:37:27 +0900

    add: 全探索の問題を解いた

--- try.py
+++ try.py
@@ -1,8 +1,5 @@
-def has_duplicates(list_data):
-  return len(list_data) != len(set(list_data))
-
 N = int(input())
+S = input()
 
-strs = [input() for _ in range(N)]
-
-print("Yes" if has_duplicates(strs) else "No")
+results = [(x, y) for x in range(N) for y in range(x + 1, N) if S[x] == S[y]]
+print(len(results))
