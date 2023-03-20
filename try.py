cb30bfb2131951618dee857ad351feff1ba5abb7
maki-engineer <ryohei-81@outlook.jp>
2023-03-21 07:26:39 +0900

    add: 全探索の問題を解いた

--- try.py
+++ try.py
@@ -1,11 +1,11 @@
-L, R = map(int, input().split())
+N = int(input())
+A = list(map(int, input().split()))
 
 count = 0
 
-for x in range(L, R + 1):
-  for y in range(x + 1, R + 1):
-    num_str1, num_str2 = str(x), str(y)
-
-    if num_str1[-1] == num_str2[-1]: count += 1
+for a in range(N):
+  for b in range(a + 1, N):
+    for c in range(b + 1, N):
+      if max(A[a], A[b], A[c]) == A[b]: count += 1
 
 print(count)
