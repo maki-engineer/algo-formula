54018ed9887c6c4cc7130b6c0195cb27d2d8b86d
maki-engineer <ryohei-81@outlook.jp>
2023-03-22 22:04:31 +0900

    add: 勤怠管理システム 失敗

--- try.py
+++ try.py
@@ -1,33 +1,29 @@
-times = [0, 0]
+times         = [0, 0]
+job_time_sums = 0
 
 for _ in range(30):
-  job_manage    = list(map(int, input().split()))
-  current_times = [0, 0]
+  job_manage = list(map(int, input().split()))
+  job_time   = 0
 
   if job_manage.count(0) == 4: continue
 
-  # 時の部分を計算
-  current_times[0] += job_manage[2] - job_manage[0]
+  # 時を加算
+  job_time += (job_manage[2] - job_manage[0]) * 60
 
-  #分の部分を計算
-  current_times[1] += abs(job_manage[1] - job_manage[3])
+  # 分を加算
+  job_time += abs(job_manage[1] - job_manage[3])
 
-  #休憩時間の差し引き
-  if (job_manage[2] - job_manage[0]) > 8:
-    current_times[0] -= 1
-  elif 6 < (job_manage[2] - job_manage[0]) <= 8:
-    if (current_times[1] - 45) < 0:
-      current_times[0] -= 1
-      current_times[1]  = 60 + (current_times[1] - 45)
-    else:
-      current_times[1] -= 45
+  # 休憩時間差し引き
+  if job_time > 480:
+    job_time -= 60
+  elif 360 < job_time <= 480:
+    job_time -= 45
 
-  #加算
-  times[0] += current_times[0]
-  times[1] += current_times[1]
+  # 加算
+  job_time_sums += job_time
 
-  if times[1] >= 60:
-    times[0] += 1
-    times[1]  = times[1] - 60
+# 変換
+times[0] = job_time_sums // 60
+times[1] = job_time_sums  % 60
 
 print(*times)
