a964a4050b77298242a9124275cf9775b010ff80
maki-engineer <ryohei-81@outlook.jp>
2023-03-22 21:53:00 +0900

    add: 勤怠管理システム 失敗

--- try.py
+++ try.py
@@ -1,14 +1,33 @@
-N = int(input())
+times = [0, 0]
 
-check = False
+for _ in range(30):
+  job_manage    = list(map(int, input().split()))
+  current_times = [0, 0]
 
-if N % 4 == 0:
-  check = True
+  if job_manage.count(0) == 4: continue
 
-  if N % 100 == 0:
-    check = False
+  # 時の部分を計算
+  current_times[0] += job_manage[2] - job_manage[0]
 
-    if N % 400 == 0:
-      check = True
+  #分の部分を計算
+  current_times[1] += abs(job_manage[1] - job_manage[3])
 
-print("Yes" if check else "No")
+  #休憩時間の差し引き
+  if (job_manage[2] - job_manage[0]) > 8:
+    current_times[0] -= 1
+  elif 6 < (job_manage[2] - job_manage[0]) <= 8:
+    if (current_times[1] - 45) < 0:
+      current_times[0] -= 1
+      current_times[1]  = 60 + (current_times[1] - 45)
+    else:
+      current_times[1] -= 45
+
+  #加算
+  times[0] += current_times[0]
+  times[1] += current_times[1]
+
+  if times[1] >= 60:
+    times[0] += 1
+    times[1]  = times[1] - 60
+
+print(*times)
