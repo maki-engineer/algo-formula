1a78e0298e7843a279a41de3e55c4e0689fb3a38
maki-engineer <ryohei-81@outlook.jp>
2023-03-22 20:39:07 +0900

    add: 同じ部屋にいた時間

--- try.py
+++ try.py
@@ -1,1 +1,11 @@
-print("Hello Algo-method!")
+sa, ta = map(int, input().split())
+sb, tb = map(int, input().split())
+
+count = 0
+
+from_time, to_time = min(sa, ta, sb, tb), max(sa, ta, sb, tb)
+
+for time in range(from_time, to_time + 1):
+  if (sa <= time) and (sb <= time) and (ta > time) and (tb > time): count += 1
+
+print(count)
