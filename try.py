cc6a53eeda7f1dce1458424d5f3cd7cbf8b0c8d4
maki-engineer <ryohei-81@outlook.jp>
2023-03-22 20:55:42 +0900

    add: マス色塗り

--- try.py
+++ try.py
@@ -1,11 +1,3 @@
-N = int(input())
+H, W, p, q = map(int, input().split())
 
-for n in range(1, N + 1):
-  if (n % 4 == 0) and (n % 6 == 0):
-    print("FizzBuzz")
-  elif n % 4 == 0:
-    print("Fizz")
-  elif n % 6 == 0:
-    print("Buzz")
-  else:
-    print(n)
+print("Black" if (p + q) % 2 == 0 else "White")
