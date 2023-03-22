dd63e601afb6f73bb735d7f2a748bc1c54d6c214
maki-engineer <ryohei-81@outlook.jp>
2023-03-22 21:01:01 +0900

    add: うるう年判定

--- try.py
+++ try.py
@@ -1,3 +1,14 @@
-H, W, p, q = map(int, input().split())
+N = int(input())
 
-print("Black" if (p + q) % 2 == 0 else "White")
+check = False
+
+if N % 4 == 0:
+  check = True
+
+  if N % 100 == 0:
+    check = False
+
+    if N % 400 == 0:
+      check = True
+
+print("Yes" if check else "No")
