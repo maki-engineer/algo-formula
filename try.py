e0b868e9300160098ebcbf30b15698a065df193b
maki-engineer <ryohei-81@outlook.jp>
2023-03-22 22:14:27 +0900

    add: 勤怠管理システム 解答参照

--- try.py
+++ try.py
@@ -1,29 +1,12 @@
-times         = [0, 0]
-job_time_sums = 0
-
-for _ in range(30):
-  job_manage = list(map(int, input().split()))
-  job_time   = 0
-
-  if job_manage.count(0) == 4: continue
-
-  # 時を加算
-  job_time += (job_manage[2] - job_manage[0]) * 60
-
-  # 分を加算
-  job_time += abs(job_manage[1] - job_manage[3])
-
-  # 休憩時間差し引き
-  if job_time > 480:
-    job_time -= 60
-  elif 360 < job_time <= 480:
-    job_time -= 45
-
-  # 加算
-  job_time_sums += job_time
-
-# 変換
-times[0] = job_time_sums // 60
-times[1] = job_time_sums  % 60
-
-print(*times)
+ans = 0
+for i in range(30):
+    H1, M1, H2, M2 = map(int, input().split())
+    if H1 == M1 == H2 == M2 == 0:
+        continue
+    m = 60 * (H2 - H1) + M2 - M1
+    if 360 < m <= 480:
+        m -= 45
+    elif m > 480:
+        m -= 60
+    ans += m
+print(ans // 60, ans % 60)
