870f152b0deecad86dc95fe9c570d0b4f53e3196
maki-engineer <ryohei-81@outlook.jp>
2023-03-24 12:06:53 +0900

    add: 約分

--- try.py
+++ try.py
@@ -1,14 +1,11 @@
-N, M      = map(int, input().split())
-menu_list = {}
-result    = 0
+nums = list(map(int, input().split("/")))
 
-for n in range(N):
-  menu = list(input().split())
-  menu_list[menu[0]] = int(menu[1])
+for n in range(2, max(nums[0], nums[1]) + 1):
+  # 約分
+  if (nums[0] % n == 0) and (nums[1] % n == 0):
+    nums[0] = nums[0] // n
+    nums[1] = nums[1] // n
+    n = 2
 
-orders = list(input().split())
-
-for order in orders:
-  result += menu_list[order]
-
-print(result)
+# 文字列に変換して結果を出力
+print("/".join(list(map(str, nums))))
