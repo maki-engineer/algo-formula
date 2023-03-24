83c69e323007217a45c89387d21ca9c01f71e6fb
maki-engineer <ryohei-81@outlook.jp>
2023-03-25 08:32:46 +0900

    add: アイスの棒

--- try.py
+++ try.py
@@ -1,11 +1,12 @@
-nums = list(map(int, input().split("/")))
+N, M   = map(int, input().split())
+result = N
 
-for n in range(2, max(nums[0], nums[1]) + 1):
-  # 約分
-  if (nums[0] % n == 0) and (nums[1] % n == 0):
-    nums[0] = nums[0] // n
-    nums[1] = nums[1] // n
-    n = 2
+while N > 0:
+  if N >= M:
+    N      -= M
+    N      += 1
+    result += 1
+  else:
+    N = 0
 
-# 文字列に変換して結果を出力
-print("/".join(list(map(str, nums))))
+print(result)
