00ca0f8bd44b9c3a1fe99ce50c41be52ac96ffa9
maki-engineer <ryohei-81@outlook.jp>
2023-03-28 10:09:42 +0900

    add: 挿入ソート

--- try.py
+++ try.py
@@ -1,21 +1,17 @@
 N = int(input())
 A = list(map(int, input().split()))
 
-for j in range(N - 1):
-  # 最小値と最小値の場所の変数
-  min_num   = 1001
-  min_index = N + 1
+for i in range(1, N):
+  pos = i
 
-  # 最小値探索
-  for k in range(j, N):
-    if min_num > A[k]:
-      min_num   = A[k]
-      min_index = k
+  # posが0になるまで昇順の入れ替え作業
+  while pos != 0:
 
-  # 交換
-  exchange     = A[j]
-  A[j]         = A[min_index]
-  A[min_index] = exchange
+    if A[pos] < A[pos - 1]:
+      exchange   = A[pos]
+      A[pos]     = A[pos - 1]
+      A[pos - 1] = exchange
 
-  #出力
+    pos -= 1
+
   print(*A)
