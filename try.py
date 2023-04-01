4037ac1b9c61b6da0fe7f75165c619e76e6e0c6e
maki-engineer <ryohei-81@outlook.jp>
2023-04-01 09:18:10 +0900

    add: マスの移動

--- try.py
+++ try.py
@@ -1,10 +1,10 @@
-N, X, Y = map(int, input().split())
+N = int(input())
+A = list(map(int, input().split()))
 
 dp    = [0 for _ in range(N)]
-dp[0] = X
-dp[1] = Y
+dp[1] = A[1]
 
 for i in range(2, N):
-  dp[i] = (dp[i - 2] + dp[i - 1]) % 100
+  dp[i] = min(A[i] + dp[i - 1], (2 * A[i]) + dp[i - 2])
 
 print(dp[-1])
